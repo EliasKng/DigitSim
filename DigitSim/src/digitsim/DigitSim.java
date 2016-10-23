edfbf49a84afc56a65e309a4279e57c80efe0f44
Tim <Tim@DESKTOP-J0FJQQP.Speedport_W_921V_1_39_000>
2016-10-23 20:21:06 +0200

    Fenstergröße erhalten

--- DigitSim/src/digitsim/DigitSim.java
+++ DigitSim/src/digitsim/DigitSim.java
@@ -6,6 +6,8 @@
 package digitsim;
 
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 
@@ -14,12 +16,23 @@ import javafx.stage.Stage;
  * @author Elias
  */
 public class DigitSim extends Application {
+    private static Stage stage;
     @Override
+  
     public void start(Stage primaryStage) {
-        Stage stage;
+        stage = primaryStage;
         stage = GenFunctions.openFXML("DigitSim.fxml", "Seminator", "icon.png");
         stage.setMinWidth(800);
-        stage.setMinHeight(600);       
+        stage.setMinHeight(600);  
+    }
+    
+    public static double[] getWindowsSize()
+    {      
+        double d[] = new double[2];
+        d[0] = stage.getWidth();
+        d[1] = stage.getHeight();
+        
+        return d;
     }
     
     /**
