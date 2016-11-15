3a3eb265d40e353cbb6da1e2c10fde476369a2b1
EliasKng <elias-koenig@web.de>
2016-11-15 19:42:00 +0100

    Start Knopf wird nun auch deaktiviert (während der Simulation)

--- DigitSim/src/digitsim/DigitSimController.java
+++ DigitSim/src/digitsim/DigitSimController.java
@@ -201,6 +201,7 @@ public class DigitSimController extends Pane{
     public void btnStartOnAction(ActionEvent event) {   //Der Startknopf dient bisher nur zur Ausgabe von Testwerten in der Konsole
         //btnStart.setDisable(true);
         btnPause.setDisable(false);
+        btnStart.setDisable(true);
         
         elements.forEach(e -> e.update()); //Geht alle Elemente durch und Updaten sie. ACHTUNG: Lambda schreibweise! Infos -> https://docs.oracle.com/javase/tutorial/java/javaOO/lambdaexpressions.html
         allConnections.update();                         
